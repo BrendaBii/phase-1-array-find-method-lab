1dce67efa9ae869f84f6611b6b345f01f042a535
BrendaBii <bii.brenda.bcb@gmail.com>
2023-06-17 11:38:39 +0300

    Assignment completed.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+let record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+]
+let sadReality =  [ { result: "N/A" }, { result: "N/A" }, { result: "N/A" } ]
+let item;
+
+function searchW (element){
+    if(element.result === "W"){
+        return true;
+    }
+}
+
+function superbowlWin(array){
+   for(item of array){
+    if(array.find(searchW) === undefined){
+        return undefined
+    }
+    else{
+        return array.find(searchW).year;
+    }
+   }
+   return undefined;
+}
+
+console.log(superbowlWin(record));
+console.log(superbowlWin(sadReality));
